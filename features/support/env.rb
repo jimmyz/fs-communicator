c216efb4bd670d8e1360008abfa1a45541e724f6
Jimmy Zimmerman <jimmy.zimmerman@gmail.com>
2009-02-21 17:28:25 -0700

    Initial commit to fs-communicator.

---
+++ features/support/env.rb
@@ -0,0 +1,11 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
+require 'fs_communicator'
+
+require 'spec/expectations'
+
+require 'test/unit/assertions'
+
+World do |world|
+  
+  world
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require 'spec'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'fs_communicator'
+
+Spec::Runner.configure do |config|
+  
+end
