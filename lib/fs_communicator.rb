5aa966bc4dad39e82ffcefdad2985df74ec25742
Jimmy Zimmerman <jimmy.zimmerman@gmail.com>
2009-03-04 13:54:08 -0700

    Added ability to post with a payload (xml)

--- lib/fs_communicator.rb
+++ lib/fs_communicator.rb
@@ -18,6 +18,23 @@ class FsCommunicator
     @session = o[:session]
   end
   
+  def post(url,payload)
+    uri = URI.parse(self.domain+url)
+    full_url = set_extra_params(uri)
+    request = Net::HTTP::Post.new(full_url)
+    request.body = payload
+    request['Content-Type'] = "text/xml"
+    http = Net::HTTP.new(uri.host, uri.port)
+    if uri.scheme == 'https'
+      http.use_ssl = true 
+      http.ca_file = File.join File.dirname(__FILE__), 'assets','entrust-ca.crt'
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    end
+    res = http.start do |ht|
+      ht.request(request)
+    end
+  end
+  
   def get(url,credentials = {})
     uri = URI.parse(self.domain+url)
     full_url = set_extra_params(uri,credentials)
@@ -38,7 +55,7 @@ class FsCommunicator
   end
   
   private
-  def set_extra_params(uri,credentials)
+  def set_extra_params(uri,credentials = {})
     if credentials[:username] && credentials[:password]
       sessionized_url = add_key(uri)
     else
--- spec/fs_communicator_spec.rb
+++ spec/fs_communicator_spec.rb
@@ -3,11 +3,13 @@ require File.dirname(__FILE__) + '/spec_helper'
 module HttpCommunicatorHelper
   
   def stub_net_objects
-    @request = mock("Net::HTTP::Get")
-    # @request.stub!(:[]=)
+    @request = mock("Net::HTTP::Get|Post")
+    @request.stub!(:[]=)
+    @request.stub!(:body=)
     @http = mock("Net::HTTP")
     Net::HTTP.stub!(:new).and_return(@http)
     Net::HTTP::Get.stub!(:new).and_return(@request)
+    Net::HTTP::Post.stub!(:new).and_return(@request)
     @http.stub!(:use_ssl=)
     @http.stub!(:ca_file=)
     @http.stub!(:verify_mode=)
@@ -110,6 +112,73 @@ describe FsCommunicator do
       }
       @http.should_receive(:start)
       do_get(@url)
+    end
+    
+  end
+  
+  describe "POST on a URL" do
+    before(:each) do
+      options = {
+        :domain => 'https://api.familysearch.org', 
+        :key => '1111-1111', 
+        :user_agent => "FsCommunicator/0.1",
+        :session => 'SESSID'
+      }
+      @com = FsCommunicator.new options
+      stub_net_objects
+      @url = '/familytree/v1/person/KWQS-BBQ'
+      @session_url = @url + "?sessionId=#{@com.session}"
+      @payload = "<familytree></familytree>"
+    end
+    
+    def do_post(url, payload = '')
+      @com.post(url, payload)
+    end
+    
+    it "should initialize a Net::HTTP object to make the request" do
+      Net::HTTP.should_receive(:new).with('api.familysearch.org',443).and_return(@http)
+      do_post(@url)
+    end
+    
+    it "should create a POST request with url containing a session" do
+      Net::HTTP::Post.should_receive(:new).with(@session_url).and_return(@request)
+      do_post(@url)
+    end
+    
+    it "should tack a sessionId as an additional parameter if params already set" do
+      url = "/familytree/v1/person/KWQS-BBQ?view=summary"
+      Net::HTTP::Post.should_receive(:new).with(url+"&sessionId=#{@com.session}").and_return(@request)
+      do_post(url)
+    end
+    
+    it "should set the request's body to the payload attached" do
+      @request.should_receive(:body=).with(@payload)
+      @request.should_receive(:[]=).with('Content-Type','text/xml')
+      do_post(@url,@payload)
+    end
+    
+    it "should set the http object to use ssl if https" do
+      @http.should_receive(:use_ssl=).with(true)
+      do_post(@url)
+    end
+    
+    it "should not set the http object to use ssl if no http" do
+      @com.domain = 'http://www.dev.usys.org'
+      @http.should_not_receive(:use_ssl=)
+      do_post(@url)
+    end
+    
+    it "should set the ca file to the entrust certificate (for FamilySearch systems)" do
+      @http.should_receive(:ca_file=).with(File.join(File.dirname(__FILE__),'..','lib','assets','entrust-ca.crt'))
+      do_post(@url)
+    end
+        
+    it "should make the request" do
+      block = lambda{ |ht|
+        ht.request('something')
+      }
+      @http.should_receive(:start)
+      do_post(@url)
     end
     
   end
